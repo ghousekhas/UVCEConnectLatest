bb880905019c3368f620d0fcae5d0aca97b720e0
Cybersmoke98 <mahith.98@gmail.com>
2018-08-04 19:19:34 +0530

    dont with about uvce layout

--- app/src/main/java/com/uvce/uvceconnect/about_uvce.java
+++ app/src/main/java/com/uvce/uvceconnect/about_uvce.java
@@ -1,11 +1,13 @@
 package com.uvce.uvceconnect;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class about_uvce extends AppCompatActivity {
 
@@ -14,11 +16,23 @@ public class about_uvce extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_uvce);
 
+        TextView card1_content =(TextView)findViewById(R.id.card1_content);
+        TextView card2_content =(TextView)findViewById(R.id.card2_content);
+        TextView card3_content =(TextView)findViewById(R.id.card3_content);
+
         //toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         //back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //customfont
+        Typeface mycustomfont = Typeface.createFromAsset(getAssets(),  "fonts/adobe_font.otf");
+
+        //customfont for the textviews in each cardview
+        card1_content.setTypeface(mycustomfont);
+        card2_content.setTypeface(mycustomfont);
+        card3_content.setTypeface(mycustomfont);
+
 
     }
 
